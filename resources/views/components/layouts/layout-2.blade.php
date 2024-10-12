544716222023cde8990032ddc822d37e950b7b19
Yves Engetschwiler <info@bee-interactive.ch>
2024-10-12 17:09:15 +0200

    Update work in progress

---
+++ resources/views/components/layouts/layout-2.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>{{ $title ?? 'Todo Manager' }}</title>
+
+        @vite('resources/css/app.css')
+    </head>
+    <body class="min-h-screen grid place-items-center p-4 bg-gray-900">
+        <div class="w-[1280px] h-[720px] bg-white">
+            <section class="overflow-hidden">
+                <div class="p-16 h-full flex flex-col justify-end">
+                    <p class="uppercase text-3xl font-bold text-red-600">{{ $subtitle }}</p>
+                    <h1 class="text-gray-900 mt-8 mb-4 text-7xl font-extrabold">{{ $title }}</h1>
+                    <p class="text-5xl">{{ $content }}</p>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
---
+++ resources/views/thumbnails/show.blade.php
@@ -0,0 +1,1 @@
+<x-layout :layout="$layout" :title="request()->input('title')" :subtitle="request()->input('subtitle')" :content="request()->input('content')"></x-layout>
